702eb18b67b3eaffe1bb1f31c117c5a6e95e3261
Sean Porter <sean.f.porter.work@gmail.com>
2025-04-18 17:35:46 +0000

    Updated to get around error about sensitive variables in for_each loops

--- backend.tf
+++ backend.tf
@@ -1,11 +1,10 @@
 terraform {
   cloud {
-    organization = "AdvancedTerraform" # CHANGE TO YOUR ORG NAME
+    organization = "SeanPorterLearning" # CHANGE TO YOUR ORG NAME
     hostname = "app.terraform.io" # Optional; defaults to app.terraform.io
 
     workspaces {
-      name = "advanced-terraform-3099246"
+      name = "advanced-terraform-seanp"
     }
   }
 }
-
